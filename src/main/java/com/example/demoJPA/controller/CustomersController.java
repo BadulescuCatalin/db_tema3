b8377900bc4ee7297c36ddf277e3bfe38d74f9e6
BadulescuCatalin <badulescucatalin01@gmail.com>
2023-08-29 16:14:58 +0300

    2nd commit

--- src/main/java/com/example/demoJPA/controller/CustomersController.java
+++ src/main/java/com/example/demoJPA/controller/CustomersController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/customer")
@@ -30,13 +31,29 @@ public class CustomersController {
         return customerService.getAllCustomers();
     }
 
+    @GetMapping(value = "/allCustomers/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable(value = "id") Integer id) {
+        Optional<Customer> optional = customerService.getCustomerById(id);
+        if (optional.isPresent()) {
+            return ResponseEntity.ok().body(optional.get());
+        }
+        return ResponseEntity.badRequest().body(null);
+    }
+
+
     @PutMapping(value = "/updateCustomer/{id}")
     public ResponseEntity<String> updateCustomerInfo(@RequestBody Customer c,
-                                                     @PathVariable Integer id) {
-        Customer updatedCustomer = customerService.updateCustomer(c);
+                                                     @PathVariable(value = "id") Integer id) {
+        Customer updatedCustomer = customerService.updateCustomer(c, id);
         if (updatedCustomer == null) {
             return ResponseEntity.badRequest().body("Invalid input");
         }
         return ResponseEntity.ok().body("Customer updated successfuly");
+    }
+
+    @DeleteMapping(value = "/deleteCustomer/{id}")
+    public ResponseEntity<String> deleteCustomer(@PathVariable(value = "id") Integer id) {
+        String message = customerService.deleteCustomerById(id);
+        return message.contains("successfuly") ? ResponseEntity.ok().body(message) : ResponseEntity.badRequest().body(message);
     }
 }
--- src/main/java/com/example/demoJPA/service/CustomerService.java
+++ src/main/java/com/example/demoJPA/service/CustomerService.java
@@ -5,6 +5,7 @@ import com.example.demoJPA.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -19,8 +20,25 @@ public class CustomerService {
         return customerRepository.findAll();
     }
 
-    public Customer updateCustomer(Customer c) {
-        return customerRepository.save(c);
+    public Customer updateCustomer(Customer c, Integer id) {
+        Optional<Customer> oldCustomer = customerRepository.findById(id);
+        if(oldCustomer.isPresent()) {
+            return customerRepository.save(c);
+        }
+        return null;
+    }
+
+    public Optional<Customer> getCustomerById(Integer id) {
+        return customerRepository.findById(id);
+    }
+
+    public String deleteCustomerById(Integer id) {
+        Optional<Customer> customer = customerRepository.findById(id);
+        if(customer.isPresent()) {
+            customerRepository.delete(customer.get());
+            return "Customer deleted successfuly";
+        }
+        return "Invalid id";
     }
 
 }
